b059f4b02be496dd7892acf2c48b85a1cca67fc5
timh52280 <timh52280@gmail.com>
2023-09-05 15:25:12 -0600

    Ultimos cambios

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { MenuItem, PrimeIcons } from 'primeng/api';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,12 +10,29 @@ import { MenuItem, PrimeIcons } from 'primeng/api';
 })
 export class NavbarComponent implements OnInit {
   menuItems: MenuItem[] | undefined;
+  menuItemHomeLabel: string | undefined;
+
+  constructor(private translate: TranslateService, private cdRef:ChangeDetectorRef) {
+
+  }
 
   ngOnInit(): void {
-    this.menuItems = [{
-      label: 'Inicio',
-      icon: PrimeIcons.HOME,
-    }];
+
+    this.translate
+    .get('Inicio').subscribe((x:string) => {
+      this.menuItems = [{
+           id: 'menu-item-home',
+           label: x,
+           icon: PrimeIcons.HOME
+         }];
+    })
+    // .subscribe((res: string) => this.menuItemHomeLabel = res);
+
+    // //  this.menuItems = [{
+    // //   id: 'menu-item-home',
+    // //   label: this.menuItemHomeLabel,
+    // //   icon: PrimeIcons.HOME,
+    // // }];
   }
   
 }
